d6431aface82445fb40d6e3e9b55a138f7cc4c4b
hour <houssem.kraoua@gmail.com>
2024-06-26 15:30:38 +0200

    add Plans common

---
+++ plans/common/base.go
@@ -0,0 +1,12 @@
+package common
+
+var (
+	ManagedFields = []string{
+		"apiVersion",
+		"kind",
+		"metadata.generation",
+		"metadata.managedFields",
+		"metadata.resourceVersion",
+		"metadata.uid",
+	}
+)
