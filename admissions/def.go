280a6027e458c39bb51e08df6529cec224652c3f
hourki <houssem.kraoua@gmail.com>
2025-02-03 23:21:32 +0100

    adjust admissions

--- admissions/def.go
+++ admissions/def.go
@@ -3,17 +3,31 @@ package admissions
 import v1 "k8s.io/api/admissionregistration/v1"
 
 type WebhookConfig struct {
-	Name          string        `json:"name"`
-	FailurePolicy EnforceType   `json:"FailurePolicy"`
-	URL           string        `json:"url"`
-	Rules         []WebhookRule `json:"rules"`
+	Name          string       `json:"name"`
+	URL           string       `json:"url"`
+	Rules         []Rule       `json:"rules"`
+	Client        ClientConfig `json:"client"`
+	FailurePolicy EnforceType  `json:"FailurePolicy"`
 }
 
-type WebhookRule struct {
+type Rule struct {
 	Operations  []v1.OperationType `json:"pperations"`
 	APIGroups   []string           `json:"apiGroups"`
 	APIVersions []string           `json:"apiVersions"`
 	Resources   []string           `json:"resources"`
+	Scope       string             `json:"scope"`
+}
+
+type ClientConfig struct {
+	Service  ServiceConfig `json:"service"`
+	CaBundle string        `json:"caBundle"`
+}
+
+type ServiceConfig struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	Path      string `json:"path"`
+	Port      int32  `json:"port"`
 }
 
 type EnforceType string
