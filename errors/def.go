4f03ad8532c67cd98727cccc602b8c9cdadfa839
hourki <houssem.kraoua@gmail.com>
2025-08-11 20:26:51 +0200

    adjust errors and messages for better clarity

--- errors/def.go
+++ errors/def.go
@@ -4,88 +4,64 @@ type Error string
 
 const (
 	// REST-related Errors
-	ErrRestMarshalPayload            Error = "error while marshaling JSON payload: %v"
-	ErrRestEmptyRequestBody          Error = "empty request body"
-	ErrRestMarshalUnstructuredToJSON Error = "error while marshaling unstructured to " +
-		"JSON: %v"
-	ErrRestUnmarshalResponseToGeneric Error = "error while unmarshaling JSON Response to " +
-		"Generic Response: %v"
-	ErrRestUnmarshalResourceToJSON Error = "error while unmarshaling resource to " +
-		"JSON: %v"
-	ErrRestUnmarshalRequestBodyToJSON Error = "error while unmarshaling request body to " +
-		"JSON: %v"
-	ErrRestSendRequest        Error = "error while sending request: %v"
-	ErrRestReadResponseBody   Error = "error while reading response body: %v"
-	ErrRestReadRequestBody    Error = "error while reading request body: %v"
-	ErrRestEncodeResponse     Error = "error while encoding response: %v"
-	ErrRestMissingRequestBody Error = "missing request body for the '{action}' " +
-		"action."
-	ErrRestParseRequestBody Error = "error while parsing request body: %v"
-	ErrRestDecodeResponse   Error = "error while decoding response: %v"
-	ErrRestMissingParam     Error = "the '{param}' parameter is required in " +
-		"the URL path."
-	ErrRestGenURL  Error = "failed to generate request URL"
-	ErrRestSendReq Error = "failed to send {method} request for"
+	ErrRestMarshalPayload             Error = "error while marshaling JSON payload: %v"
+	ErrRestEmptyRequestBody           Error = "empty request body"
+	ErrRestMarshalUnstructuredToJSON  Error = "error while marshaling unstructured to JSON: %v"
+	ErrRestUnmarshalResponseToGeneric Error = "error while unmarshaling JSON Response to Generic Response: %v"
+	ErrRestUnmarshalResourceToJSON    Error = "error while unmarshaling resource to JSON: %v"
+	ErrRestUnmarshalRequestBodyToJSON Error = "error while unmarshaling request body to JSON: %v"
+	ErrRestSendRequest                Error = "error while sending request: %v"
+	ErrRestReadResponseBody           Error = "error while reading response body: %v"
+	ErrRestReadRequestBody            Error = "error while reading request body: %v"
+	ErrRestEncodeResponse             Error = "error while encoding response: %v"
+	ErrRestMissingRequestBody         Error = "missing request body for the '{action}' action"
+	ErrRestParseRequestBody           Error = "error while parsing request body: %v"
+	ErrRestDecodeResponse             Error = "error while decoding response: %v"
+	ErrRestRequiredParam              Error = "the %s parameter is required in the URL path"
+	ErrRestGenURL                     Error = "failed to generate request URL"
+	ErrRestSendReqWithWrongMeth       Error = "failed to send request with method %s"
 
 	// K8s-related Errors
-	ErrK8sSetClient    Error = "failed to set Kubernetes client: %v"
-	ErrK8sCreateConfig Error = "failed to create Kubernetes in-cluster " +
-		"config: %v"
-	ErrK8sGetService Error = "error getting Service %s in Namespace " +
-		"%s: %v"
-	ErrK8sEmptyNamespaceOrResourceName Error = "namespace or resourceName cannot be empty"
-	ErrK8sEmptyNamespace               Error = "Namespace cannot be empty"
-	ErrK8sFetchingServices             Error = "error fetching services in namespace " +
-		"%s: %v"
-	ErrK8sFetchingDeployments Error = "error fetching deployments in namespace " +
-		"%s: %v"
-	ErrK8sFetchingPodEvents Error = "error fetching events for pod %s in " +
-		"namespace %s: %v"
+	ErrK8sSetClient           Error = "failed to set Kubernetes client: %v"
+	ErrK8sCreateConfig        Error = "failed to create Kubernetes in-cluster config: %v"
+	ErrK8sGetService          Error = "error getting Service %s in Namespace %s: %v"
+	ErrK8sEmptyNsOrResName    Error = "namespace or resourceName cannot be empty"
+	ErrK8sEmptys              Error = "namespace cannot be empty"
+	ErrK8sFetchingServices    Error = "error fetching services in namespace %s: %v"
+	ErrK8sFetchingDeployments Error = "error fetching deployments in namespace %s: %v"
+	ErrK8sFetchingPodEvents   Error = "error fetching events for pod %s in namespace %s: %v"
 
 	// Resource-related Errors
-	ErrCreateResource Error = "error while creating resource %s"
-	ErrGetResource    Error = "error while fetching resource " +
-		"%s: %v"
-	ErrListResources  Error = "error while listing resources: %v"
-	ErrUpdateResource Error = "error while updating resource " +
-		"%s: %v"
-	ErrDeleteResource Error = "error while deleting resource " +
-		"%s: %v"
-	ErrUpdateResourceWithHistory Error = "error while updating resource " +
-		"with history."
-	ErrUpdateResourceSync Error = "error while updating resource " +
-		"sync settings."
-	ErrFilterResources        Error = "error while filtering resource: %v"
-	ErrCheckResourceExistence Error = "error while checking resource " +
-		"existence"
-	ErrResourceExists   Error = "resource already exists"
-	ErrResourceNotFound Error = "resource not found"
-	ErrPatchResource    Error = "error while patching resource " +
-		"%s status: %s"
-	ErrConvertResource Error = "error while converting resource " +
-		"to %s: %v"
-	ErrResourceHasNoFields      Error = "%s has no %s"
-	ErrRemoveFieldsFromResource Error = "error while removing %s from %s"
-	ErrGenerateManagedResource  Error = "error while generating managed " +
-		"resource %s: %v"
-	ErrUnsupportedResourceType Error = "unsupported resource type %s: %v"
-	ErrAddResourceToGrouper    Error = "error while adding resource " +
-		"%s to grouper %s: %v"
-	ErrDeleteOperationTimedOut Error = "delete operation timed out"
-	ErrRemoveFromGrouper       Error = "failed to remove %s from " +
-		"grouper %s: %v"
-	ErrRemoveFromAttachedResources Error = "failed to remove %s from " +
-		"attached resource: %v"
-	ErrBothGroupersMustBeProvidedForComparison Error = "both groupers must be provided " +
-		"for comparison"
-	ErrMaintenanceObjectIsNil Error = "maintenance object is nil"
+	ErrCreateRes                               Error = "error while creating resource %s: %v"
+	ErrGetRes                                  Error = "error while fetching resource %s: %v"
+	ErrListRes                                 Error = "error while listing resources: %v"
+	ErrUpdateRes                               Error = "error while updating resource %s: %v"
+	ErrDeleteRes                               Error = "error while deleting resource %s: %v"
+	ErrUpdateResWithHistory                    Error = "error while updating resource with history"
+	ErrUpdateResSync                           Error = "error while updating resource sync settings"
+	ErrFilterRes                               Error = "error while filtering resource: %v"
+	ErrCheckResExistence                       Error = "error while checking resource existence"
+	ErrResExists                               Error = "resource already exists"
+	ErrResNotFound                             Error = "resource not found"
+	ErrPatchRes                                Error = "error while patching resource %s status: %s"
+	ErrConvertRes                              Error = "error while converting resource to %s: %v"
+	ErrResHasNoFields                          Error = "%s has no %s"
+	ErrRemoveFieldsFromRes                     Error = "error while removing %s from %s"
+	ErrGenerateManagedRes                      Error = "error while generating managed resource %s: %v"
+	ErrUnsupportedResType                      Error = "unsupported resource type %s: %v"
+	ErrAddResToGrouper                         Error = "error while adding resource %s to grouper %s: %v"
+	ErrDeleteOperationTimedOut                 Error = "delete operation timed out"
+	ErrRemoveFromGrouper                       Error = "failed to remove %s from grouper %s: %v"
+	ErrRemoveFromAttachedRes                   Error = "failed to remove %s from attached resource: %v"
+	ErrBothGroupersMustBeProvidedForComparison Error = "both groupers must be provided for comparison"
+	ErrMaintenanceObjectIsNil                  Error = "maintenance object is nil"
 
 	// Build-related Errors
 	ErrGrouperCannotBeNil           Error = "grouper cannot be nil"
 	ErrGrouperCannotBeEmpty         Error = "grouper cannot be empty"
 	ErrSourceNameCannotBeEmpty      Error = "source name cannot be empty"
 	ErrResourceNameCannotBeEmpty    Error = "resource name cannot be empty"
-	ErrUnsupportedAppWorkloadType   Error = "unsupported App Workload Type: %v" //nolint:gosec
+	ErrUnsupportedAppWorkloadType   Error = "unsupported App Workload Type: %v"
 	ErrUnsupportedBatchWorkloadType Error = "unsupported Batch Workload Type: %v"
 
 	// Patch-related Errors
@@ -94,28 +70,14 @@ const (
 	ErrGroupersResourcesEmptyOrNil   Error = "groupers resources are empty or nil"
 
 	// Admission-related Errors
-	ErrCreateValidatingAdmission Error = "error while creating admission validating " +
-		"webhook."
-	ErrCreateMutatingAdmission Error = "error while creating admission mutating " +
-		"webhook."
-	ErrGetValidatingAdmission Error = "error while fetching admission validating " +
-		"webhook."
-	ErrGetMutatingAdmission Error = "error while fetching admission mutating " +
-		"webhook."
-	ErrUpdateValidatingAdmission Error = "error while updating admission validating " +
-		"webhook."
-	ErrUpdateMutatingAdmission Error = "error while updating admission mutating " +
-		"webhook."
-	ErrDeleteValidatingAdmission Error = "error while deleting admission validating " +
-		"webhook."
-	ErrDeleteMutatingAdmission Error = "error while deleting admission mutating " +
-		"webhook."
-	ErrFilterAdmission         Error = "error while filtering admission webhook."
-	ErrCheckAdmissionExistence Error = "error while checking admission webhook " +
-		"existence."
-	ErrAdmissionValidatingExists Error = "admission validating webhook already exists."
-	ErrAdmissionMutatingExists   Error = "admission mutating webhook already exists."
-	ErrAdmissionNotFound         Error = "admission webhook not found."
+	ErrCreateAdmission         Error = "error while creating admission %s:%s-webhook"
+	ErrGetAdmission            Error = "error while fetching admission %s:%s-webhook"
+	ErrUpdateAdmission         Error = "error while updating admission %s:%s-webhook"
+	ErrDeleteAdmission         Error = "error while deleting admission %s:%s-webhook"
+	ErrFilterAdmission         Error = "error while filtering admission %s:%s-webhook"
+	ErrCheckAdmissionExistence Error = "error while checking admission %s:%s-webhook existence"
+	ErrAdmissionExists         Error = "admission %s:%s-webhook already exists"
+	ErrAdmissionNotFound       Error = "admission %s:%s-webhook not found"
 
 	// NATS-related Errors
 	ErrNatsAuth                               Error = "user and password must be set for auth"
@@ -152,11 +114,9 @@ const (
 	ErrNatsNoParsedMessageFoundInMetadata     Error = "no parsed message found in metadata"
 	ErrNatsNoScopeFoundInUpdateMessage        Error = "no scope found in update message"
 	ErrNatsAckError                           Error = "failed to acknowledge: %v"
-	ErrNatsNoFasidOrCacidDataFound            Error = "no fasid or cacid data found in " +
-		"message for full scope"
-	ErrNatsUnknownScope                     Error = "unknown scope: %s"
-	ErrNatsCouldNotDetermineResNameFromData Error = "could not determine %s name from " +
-		"message data"
+	ErrNatsNoFasidOrCacidDataFound            Error = "no fasid or cacid data found in message for full scope"
+	ErrNatsUnknownScope                       Error = "unknown scope: %s"
+	ErrNatsCouldNotDetermineResNameFromData   Error = "could not determine %s name from message data"
 	ErrNatsNoResourceNameFoundInDeleteMessage Error = "no resourceName found in delete message"
 	ErrNatsFailedToMarshalMsgData             Error = "failed to marshal message data: %v"
 	ErrNatsFailedToUnmarshalMsgData           Error = "failed to unmarshal message data: %v"
--- messages/def.go
+++ messages/def.go
@@ -3,50 +3,38 @@ package messages
 type Message string
 
 const (
-	SuccessListResources             Message = "resources listed successfully."
-	SuccessGetResource               Message = "resource %s of kind %s fetched successfully."
-	SuccessCreateResource            Message = "resource %s of kind %s created successfully."
-	SuccessUpdateResource            Message = "resource %s of kind %s updated successfully."
-	SuccessPatchResource             Message = "resource %s of kind %s patched successfully."
-	SuccessDeleteResource            Message = "resource %s of kind %s deleted successfully."
-	InfoResourceCreateWithOutHistory Message = "resource %s of kind %s created without " +
-		"add in history."
-	InfoResourceUpdateWithOutHistory Message = "resource %s of kind %s updated without " +
-		"add in history."
-	InfoResourcePatchWithOutHistory Message = "resource %s of kind %s patched without " +
-		"add in history."
-	InfoResourceDeleteWithOutHistory Message = "resource %s of kind %s deleted without " +
-		"add in history."
+	// Resource-related Messages
+	SuccessListRes                  Message = "resources listed successfully"
+	SuccessGetRes                   Message = "resource %s of kind %s fetched successfully"
+	SuccessCreateRes                Message = "resource %s of kind %s created successfully"
+	SuccessUpdateRes                Message = "resource %s of kind %s updated successfully"
+	SuccessPatchRes                 Message = "resource %s of kind %s patched successfully"
+	SuccessDeleteRes                Message = "resource %s of kind %s deleted successfully"
+	InfResCreateWithoutHistory      Message = "resource %s of kind %s created without add in history"
+	InfResUpdateWithoutHistory      Message = "resource %s of kind %s updated without add in history"
+	InfResPatchWithoutHistory       Message = "resource %s of kind %s patched without add in history"
+	InfResDeleteWithoutHistory      Message = "resource %s of kind %s deleted without add in history"
+	InfBridgeOrWorkloadsAreEmpty    Message = "bridge or workloads are empty"
+	InfBatchWorkloadPatchNotSuppYet Message = "batch workload patching operation not supported yet"
+	InfSkipPatchEmptyAppsOrBridges  Message = "skipping patching: empty apps or bridges collection"
+	SuccessRecordResCreated         Message = "resource created successfully"
+	SuccessRecordResUpdated         Message = "resource updated successfully"
 
-	// Validating Admission Webhook Messages
-	SuccessCreateValidatingAdmission Message = "admission validating webhook created " +
-		"successfully."
-	SuccessGetValidatingAdmission Message = "admission validating webhook fetched " +
-		"successfully."
-	SuccessUpdateValidatingAdmission Message = "admission validating webhook updated " +
-		"successfully."
-	SuccessDeleteValidatingAdmission Message = "admission validating webhook deleted " +
-		"successfully."
+	// Admission-related Messages
+	SuccessCreateAdmission Message = "admission %s:%s-webhook created successfully"
+	SuccessGetAdmission    Message = "admission %s:%s-webhook fetched successfully"
+	SuccessUpdateAdmission Message = "admission %s:%s-webhook updated successfully"
+	SuccessDeleteAdmission Message = "admission %s:%s-webhook deleted successfully"
 
-	// Mutating Admission Webhook Messages
-	SuccessCreateMutatingAdmission Message = "admission mutating webhook created " +
-		"successfully."
-	SuccessGetMutatingAdmission Message = "admission mutating webhook fetched " +
-		"successfully."
-	SuccessUpdateMutatingAdmission Message = "admission mutating webhook updated " +
-		"successfully."
-	SuccessDeleteMutatingAdmission Message = "admission mutating webhook deleted " +
-		"successfully."
+	// Status Messages
+	SuccessOperation Message = "operation completed successfully"
+	SuccessCreation  Message = "creation completed successfully"
+	SuccessDeletion  Message = "deletion completed successfully"
+	SuccessUpdate    Message = "update completed successfully"
 
-	// General Status Messages
-	SuccessOperation Message = "operation completed successfully."
-	SuccessCreation  Message = "creation completed successfully."
-	SuccessDeletion  Message = "deletion completed successfully."
-	SuccessUpdate    Message = "update completed successfully."
-
-	// NATS
-	SuccessNatsConnectionStatusConnected    Message = "nats connection status: connected"
-	SuccessNatsConnectionStatusDisconnected Message = "nats connection status: disconnected"
+	// NATS-related Messages
+	SuccessNatsConnectionStatusConnected    Message = "nats status: connected"
+	SuccessNatsConnectionStatusDisconnected Message = "nats status: disconnected"
 	SuccessNatsTopicSubscribe               Message = "successfully subscribed to topic %s"
 	SuccessNatsTopicPublish                 Message = "successfully published to topic %s"
 	SuccessNatsTopicMessageReceive          Message = "received message on topic %s"
@@ -63,24 +51,10 @@ const (
 	SuccessNatsPatchWorkload                Message = "successfully patched workload %s"
 	SuccessNatsPatchBridge                  Message = "successfully patched bridge %s"
 
-	// Common Server Messages
-	SuccessReceivedShutdownSig Message = "received shutdown signal, cleaning up..."
-	SuccessContextCanceled     Message = "context canceled, cleaning up..."
-	SuccessStartingServer      Message = "starting server on port"
-
-	// Health Monitoring
+	// Server-related Messages
+	SuccessReceivedShutdownSig      Message = "received shutdown signal, cleaning up..."
+	SuccessContextCanceled          Message = "context canceled, cleaning up..."
+	SuccessStartingServer           Message = "starting server on port"
 	SuccessServiceHealthCheckPassed Message = "service health check passed"
-	SuccessServiceRestarting        Message = "restarting service due to health check " +
-		"failure..."
-
-	// Patch-related Messages
-	InfoBridgeOrWorkloadsAreEmpty            Message = "bridge or workloads are empty"
-	InfoBatchWorkloadPatchingNotSupportedYet Message = "batch workload patching " +
-		"operation not supported yet"
-	InfoSkippingPatchingEmptyAppsOrBridgesCollection Message = "skipping patching: empty " +
-		"apps or bridges collection."
-
-	// History Records
-	RecordResourceCreated Message = "resource created successfully"
-	RecordResourceUpdated Message = "resource updated successfully"
+	SuccessServiceRestarting        Message = "restarting service due to health check failure..."
 )
