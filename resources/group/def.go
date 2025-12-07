713d1cb819a21488d929c4f0320e9024b112b411
hourki <houssem.kraoua@gmail.com>
2025-12-07 21:54:39 +0100

    extend group resource

--- resources/group/def.go
+++ resources/group/def.go
@@ -1,11 +1,13 @@
 package group
 
 type GroupAsResource struct {
-	ID             string  `json:"id"`
-	Name           string  `json:"name"`
-	Description    string  `json:"description"`
-	CategoryID     string  `json:"categoryID"`
-	CreationDate   string  `json:"creationDate"`
-	LastUpdateDate *string `json:"lastUpdateDate,omitempty"`
+	ID               string   `json:"id"`
+	AssignedUsersIDs []string `json:"assignedUsersIDs"`
+	Name             string   `json:"name"`
+	Description      string   `json:"description"`
+	CategoryID       string   `json:"categoryID"`
+	CreationDate     string   `json:"creationDate"`
+	LastUpdateDate   *string  `json:"lastUpdateDate,omitempty"`
+	CreatedBy        *string  `json:"createdBy,omitempty"`
+	LastUpdatedBy    *string  `json:"lastUpdatedBy,omitempty"`
 }
-
