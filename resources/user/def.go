8945e5a921811c57c56b34df128bff33452e9f56
hourki <houssem.kraoua@gmail.com>
2026-04-21 16:13:17 +0200

    extend user model to support identities for external OIDC

--- resources/user/def.go
+++ resources/user/def.go
@@ -1,16 +1,23 @@
 package user
 
 type UserAsResource struct {
-	ID                string     `json:"id"`
-	Username          string     `json:"username"`
-	Fullname          string     `json:"fullname"`
-	Email             string     `json:"email"`
-	AssignedRolesIDs  []*string  `json:"assignedRolesIDs,omitempty"`
-	AssignedGroupsIDs []*string  `json:"assignedGroupsIDs,omitempty"`
-	CreationDate      string     `json:"creationDate"`
-	LastUpdateDate    *string    `json:"lastUpdateDate,omitempty"`
-	Avatar            *Avatar    `json:"avatar,omitempty"`
-	Status            UserStatus `json:"status"`
+	ID                string          `json:"id"`
+	Username          string          `json:"username"`
+	Fullname          string          `json:"fullname"`
+	Email             string          `json:"email"`
+	AssignedRolesIDs  []*string       `json:"assignedRolesIDs,omitempty"`
+	AssignedGroupsIDs []*string       `json:"assignedGroupsIDs,omitempty"`
+	Identities        []*UserIdentity `json:"identities,omitempty"`
+	CreationDate      string          `json:"creationDate"`
+	LastUpdateDate    *string         `json:"lastUpdateDate,omitempty"`
+	Avatar            *Avatar         `json:"avatar,omitempty"`
+	Status            UserStatus      `json:"status"`
+}
+
+type UserIdentity struct {
+	Provider string `json:"provider,omitempty"`
+	Issuer   string `json:"issuer,omitempty"`
+	Subject  string `json:"subject,omitempty"`
 }
 
 type Avatar struct {
